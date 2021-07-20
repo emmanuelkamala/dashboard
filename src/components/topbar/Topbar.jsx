f7d5de5807c1290ca69742cb1184cdd71c11440a
emmanuelkamala <emmanuelkamala86@gmail.com>
2021-07-21 02:01:23 +0300

    Create a topbar component

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,12 +1,28 @@
 import React from 'react';
 import './topbar.css';
+import { Language, NotificationsNone, Settings } from '@material-ui/icons';
 
 const Topbar = () => {
   return (
     <div className='topbar'>
      <div className="topbarWrapper">
-       <div className="topleft"></div>
-       <div className="topright"></div>
+       <div className="topleft">
+         <span className='logo'>Admin Dashboard</span>
+       </div>
+       <div className="topright">
+         <div className="topbarIconContainer">
+           <NotificationsNone />
+           <span className='topIconBadge'>2</span>
+         </div>
+         <div className="topbarIconContainer">
+           <Language />
+           <span className='topIconBadge'>2</span>
+         </div>
+         <div className="topbarIconContainer">
+           <Settings />
+         </div>
+         <img src="./images/ejoka.png" alt="profile" className="topAvatar" />
+       </div>
      </div>
     </div>
   )
