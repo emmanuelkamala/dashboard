0c9fa0b8dc565aa8577e8c672deaa73c8f3fc89f
emmanuelkamala <emmanuelkamala86@gmail.com>
2021-07-21 01:04:40 +0300

    Initial app files

---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './topbar.css';
+
+const Topbar = () => {
+  return (
+    <div className='topbar'>
+     <div className="topbarWrapper">
+       <div className="topleft"></div>
+       <div className="topright"></div>
+     </div>
+    </div>
+  )
+}
+
+export default Topbar;
