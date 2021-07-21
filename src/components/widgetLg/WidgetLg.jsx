89cb5c6c8e81f3e23f9650f4464140ab9b98ca9c
emmanuelkamala <emmanuelkamala86@gmail.com>
2021-07-21 23:27:55 +0300

    Create widgets components

--- src/components/widgetLg/WidgetLg.jsx
+++ src/components/widgetLg/WidgetLg.jsx
@@ -1,9 +1,57 @@
 import './widgetLg.css';
 
 const WidgetLg = () => {
+  const Button = ({type}) => {
+    return <button className={'widgetLgButton ' + type}>{type}</button>
+  }
   return (
     <div className='widgetLg'>
-      Large
+      <h3 className='widgetLgTitle'>Latest Transactions</h3>
+      <table className="widgetLgTable">
+        <tr className="widgetLgTr">
+          <th className="widgetLgTh">Customer</th>
+          <th className="widgetLgTh">Date</th>
+          <th className="widgetLgTh">Amount</th>
+          <th className="widgetLgTh">Status</th>
+        </tr>
+        <tr className="widgetLgTr">
+          <td className="widgetLgUser">
+            <img src="/images/ejoka.png" alt="profile" className="widgetLgImg" />
+            <span className="widgetLgName">Emmanuel Joatham</span>
+          </td>
+          <td className="widgetLgDate">21st Jul 2021</td>
+          <td className="widgetLgAmount">$250.00</td>
+          <td className="widgetLgStatus"><Button type='Approved' /></td>
+        </tr>
+        <tr className="widgetLgTr">
+          <td className="widgetLgUser">
+            <img src="/images/ejoka.png" alt="profile" className="widgetLgImg" />
+            <span className="widgetLgName">Emmanuel Joatham</span>
+          </td>
+          <td className="widgetLgDate">21st Jul 2021</td>
+          <td className="widgetLgAmount">$250.00</td>
+          <td className="widgetLgStatus"><Button type='Pending' /></td>
+        </tr>
+        <tr className="widgetLgTr">
+          <td className="widgetLgUser">
+            <img src="/images/ejoka.png" alt="profile" className="widgetLgImg" />
+            <span className="widgetLgName">Emmanuel Joatham</span>
+          </td>
+          <td className="widgetLgDate">21st Jul 2021</td>
+          <td className="widgetLgAmount">$250.00</td>
+          <td className="widgetLgStatus"><Button type='Declined' /></td>
+        </tr>
+        <tr className="widgetLgTr">
+          <td className="widgetLgUser">
+            <img src="/images/ejoka.png" alt="profile" className="widgetLgImg" />
+            <span className="widgetLgName">Emmanuel Joatham</span>
+          </td>
+          <td className="widgetLgDate">21st Jul 2021</td>
+          <td className="widgetLgAmount">$250.00</td>
+          <td className="widgetLgStatus"><Button type='Approved' /></td>
+        </tr>
+        
+      </table>
     </div>
   )
 }
