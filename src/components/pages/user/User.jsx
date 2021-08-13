405ebc5c00380a1af71b1d9ca4ae367a556b7679
emmanuelkamala <emmanuelkamala86@gmail.com>
2021-08-13 14:05:44 +0300

    Add link to create a new user

--- src/components/pages/user/User.jsx
+++ src/components/pages/user/User.jsx
@@ -1,4 +1,5 @@
 import { CalendarToday, EmailOutlined, LocationSearching, PermIdentity, PhoneAndroid, Publish } from '@material-ui/icons';
+import { Link } from 'react-router-dom';
 import './user.css';
 
 function User() {
@@ -6,7 +7,9 @@ function User() {
     <div className='user'>
       <div className="userTitleContainer">
         <h1 className='userTitle'>Edit User</h1>
-        <button className='userAddButton'>Create</button>
+        <Link to='/newUser'>
+          <button className='userAddButton'>Create</button>
+        </Link>  
       </div>
       <div className="userContainer">
         <div className="userShow">
