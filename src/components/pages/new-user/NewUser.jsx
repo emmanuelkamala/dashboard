e83634c1adbdc013ff8980d5336276d6567a5410
emmanuelkamala <emmanuelkamala86@gmail.com>
2021-08-13 14:15:50 +0300

    Create a form on new user component

--- src/components/pages/new-user/NewUser.jsx
+++ src/components/pages/new-user/NewUser.jsx
@@ -5,7 +5,33 @@ import React from 'react'
 function NewUser() {
   return (
     <div className='newUser'>
-      new user
+      <h1 className="newUserTitle">New User</h1>
+      <form className="newUserForm">
+        <div className="newUserItem">
+          <label>Username</label>
+          <input type="text" placeholder="Username" />
+        </div>
+        <div className="newUserItem">
+          <label>Full Name</label>
+          <input type="text" placeholder="First and Last Name" />
+        </div>
+        <div className="newUserItem">
+          <label>Email</label>
+          <input type="email" placeholder="Email" />
+        </div>
+        <div className="newUserItem">
+          <label>Password</label>
+          <input type="password" placeholder="Password" />
+        </div>
+        <div className="newUserItem">
+          <label>Phone</label>
+          <input type="text" placeholder="Phone number" />
+        </div>
+        <div className="newUserItem">
+          <label>Address</label>
+          <input type="text" placeholder="Enter your address" />
+        </div>
+      </form>
     </div>
   )
 }
